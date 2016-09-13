8093ea22a027d0b914ae57a2a98e4160c87f9eda
Satmage <kleckers@yandex.ru>
2016-09-13 19:01:35 +0300

    Создан класс image с конструкстором

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+Class image {
+	public $sql;
+	public $text;
+ 	
+ 	public	function __construct($sql='') {
+ 		
+ 	}
+
+ 	public function view() {
+ 	
+ 	}
+}
+
+$image = new image();
+var_dump($image);
+
+?>
