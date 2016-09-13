2260925c5752a8b84b751f7b347a5c10ba5d2efc
Satmage <kleckers@yandex.ru>
2016-09-13 19:33:18 +0300

    1. Конструктор подключается к БД 2. Создан метод для передачи запроса к БД

--- index.php
+++ index.php
@@ -1,31 +1,47 @@
 <?php
 
+require_once __DIR__.'/sql.php';
+
 Class image {
+	public $title;
 	public $sql;
-	public $text;
  	
- 	public	function __construct($sql='') {
+ 	// Конструктор подключается к БД
+ 	public	function __construct() {
+ 		mysql_connect('localhost','root','');
+ 		mysql_select_db('gallery');	
+ 	}
+
+ 	// Метод выполняет запрос к БД
+ 	public function sql_query($sql) {
+ 		$res = mysql_query($sql);
+ 		$ret = [];
+
+ 		while(false !== ($row = mysql_fetch_assoc($res))) {
+ 			$ret[] = $row;
+ 		}
+
+ 		return $ret;
+ 	} 
+
+ 	public function photo_getAll() {
  		
  	}
 
- 	public function Photo_getAll() {
+ 	public function photo_add() {
 
  	}
 
- 	public function add_photo() {
+ 	public function photo_update() {
 
  	}
 
- 	public function update_photo() {
-
- 	}
-
- 	public function delete_photo() {
+ 	public function photo_delete() {
 
  	}
 }
 
-$image = new image();
-var_dump($image);
+$image = new image(sql_connect());
+$image->sql_query('SELECT * FROM image');
 
 ?>
