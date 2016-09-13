cdce001c8ae617f52709447c0d6a05ae52de9b19
Satmage <kleckers@yandex.ru>
2016-09-13 21:11:04 +0300

    Форма для загрузки файлов в БД

---
+++ views/form.php
@@ -0,0 +1,7 @@
+<form action="./add.php" method="post">
+	<label for='title'>Название</label>
+	<input type="text" id='title' name="title">
+	<label for='image'>Файл</label>
+	<input type="file" id='image' name="image">
+	<input type="submit" name="">
+</form>
