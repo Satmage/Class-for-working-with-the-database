c8007fdb183401efeb9755076b3d2f8ff66801d6
Satmage <kleckers@yandex.ru>
2016-09-13 20:57:38 +0300

    Отображение

---
+++ views/index.php
@@ -0,0 +1,26 @@
+<?php include __DIR__.'/index.php'; ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+<form action="index.php" method="post">
+	<table>
+		<tr>
+			<th>Наименование</th>
+			<th>Фото</th>
+		</tr>
+		<tr>
+			
+		</tr>
+	</table>
+
+	<?php include __DIR__ . '/form.php'; ?>
+
+</form>
+
+</body>
+</html>
